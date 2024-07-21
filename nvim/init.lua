f4f8cfc36af69937880b9cdd83743098126809e8
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-07-21 15:59:15 -0500

    Made neovim sentient!

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,9 @@
+print("Welcome back, commander Jean")		--Making neovim sentient
 vim.cmd("set number")				--Show numbers
 vim.cmd("set scroll=10")			--Set scroll to 10(ctrl + u to go up/ctrl + d to go down)
 vim.cmd("colorscheme industry") 		--set colorscheme for the editor
 vim.cmd("set statusline=%F")			--Modify the statusline 
 vim.cmd("set cursorline")			--Show cursorline
-
 
 
 
