e5c10863820e061f59546c4d6401fcfda4872e93
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-07-21 15:34:11 -0500

    Set cursorline

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,7 @@ vim.cmd("set number")				--Show numbers
 vim.cmd("set scroll=10")			--Set scroll to 10(ctrl + u to go up/ctrl + d to go down)
 vim.cmd("colorscheme industry") 		--set colorscheme for the editor
 vim.cmd("set statusline=%F")			--Modify the statusline 
-
+vim.cmd("set cursorline")			--Show cursorline
 
 
 
