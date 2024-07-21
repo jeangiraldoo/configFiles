7be0bcc7e02a093ed07363c2e242a6fc0dd4ce4f
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-07-21 15:30:20 -0500

    Add comments to the lua code

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,54 @@
-vim.cmd("set number")
-vim.cmd("set scroll=10")
-vim.cmd("colorscheme industry") 
-vim.cmd("set statusline=%F")
+vim.cmd("set number")				--Show numbers
+vim.cmd("set scroll=10")			--Set scroll to 10(ctrl + u to go up/ctrl + d to go down)
+vim.cmd("colorscheme industry") 		--set colorscheme for the editor
+vim.cmd("set statusline=%F")			--Modify the statusline 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
