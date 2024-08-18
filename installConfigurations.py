5e50e36ce026630025c004729e1b4beb47a836c0
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-08-18 12:10:30 -0500

    Implement a Hashmap to store the file names and the paths where they should be moved to. I also added functionality to move files to a specified path

--- installConfigurations.py
+++ installConfigurations.py
@@ -1,10 +1,8 @@
-import platform
-import os
-
-def main():
-    print("Configuration files installation started!")
-
-    operatingSystem = platform.system()
+import platform 
+import os 
+def main(): 
+    print("Configuration files installation started!") 
+    operatingSystem = platform.system() 
     user = os.getlogin()
     print("Initiating Operating system detection...")
 
@@ -15,7 +13,10 @@ def main():
 
 def windowsInstallation(user):
     createWindowsProgrammingDirectory(user)
-    
+    files = {"init.lua":f"C:/Users/{user}/Desktop"}
+    for fileName in(files):
+        moveConfigurationFile(fileName, files[fileName]) 
+
 def createDirectory(path):
     exists = os.path.exists(path)
     if(exists == False):
@@ -24,12 +25,16 @@ def createDirectory(path):
     else:
         print(f"Error. Directory '{path}' already exists")
 
+def moveConfigurationFile(fileName, path):
+    print(f"Moving {fileName} to {path}")
+    print(path)
+    os.system(f"move {fileName} {path}")
+
 def createWindowsProgrammingDirectory(user):
     print("Creating directories...")
         
     path = f"C:/Users/{user}/Documents/programming/configFiles"
     createDirectory(path)
-
 
 main()
 
